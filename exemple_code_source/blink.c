8f29dd11a3ee30b622174ed1cee7c908d1a2e303
Jérémy Cheynet <jeremy@minet.net>
2010-10-14 23:42:56 +0200

    Rajout du système de compilation pour les 2 types de carte (arduino et arduino mega)

--- exemple_code_source/blink.c
+++ exemple_code_source/blink.c
@@ -3,14 +3,22 @@
 
 #define TRUE 1
 
+#ifdef ARDUINO
+	#define PORT_SORTIE PORTB5
+#endif
+#ifdef ARDUINO_MEGA
+	#define PORT_SORTIE PORTB7
+#endif
+
 int main( void )
 {
-	DDRB |= ( 1 << PORTB5 );
+
+	DDRB |= ( 1 << PORT_SORTIE );
 	while( TRUE )
 	{
-		PORTB |= ( 1 << PORTB5 );
+		PORTB |= ( 1 << PORT_SORTIE );
 		_delay_ms( 1000 );
-		PORTB &= ~( 1 << PORTB5 );
+		PORTB &= ~( 1 << PORT_SORTIE );
 		_delay_ms( 2000 );
 	}
 	return 0;
